5573e5b4c71746e0165dda37c6427f3ba31ffbfb
Kenna Wang <kenna.wang6@gmail.com>
2018-11-18 01:37:05 -0800

    Added main-scene.js, dependencies.js, and tiny-graphics.js. Created starting box for vending machine.

---
+++ main-scene.js
@@ -0,0 +1,89 @@
+window.Cube = window.classes.Cube =
+class Cube extends Shape                 // Here's a complete, working example of a Shape subclass.  It is a blueprint for a cube.
+  { constructor()
+      { super( "positions", "normals" ); // Name the values we'll define per each vertex.  They'll have positions and normals.
+
+        // First, specify the vertex positions -- just a bunch of points that exist at the corners of an imaginary cube.
+        this.positions.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [-1,-1,1], [1,-1,1], [1,1,-1],  [-1,1,-1],  [1,1,1],  [-1,1,1],
+                                          [-1,-1,-1], [-1,-1,1], [-1,1,-1], [-1,1,1], [1,-1,1],  [1,-1,-1],  [1,1,1],  [1,1,-1],
+                                          [-1,-1,1],  [1,-1,1],  [-1,1,1],  [1,1,1], [1,-1,-1], [-1,-1,-1], [1,1,-1], [-1,1,-1] ) );
+        // Supply vectors that point away from eace face of the cube.  They should match up with the points in the above list
+        // Normal vectors are needed so the graphics engine can know if the shape is pointed at light or not, and color it accordingly.
+        this.normals.push(   ...Vec.cast( [0,-1,0], [0,-1,0], [0,-1,0], [0,-1,0], [0,1,0], [0,1,0], [0,1,0], [0,1,0], [-1,0,0], [-1,0,0],
+                                          [-1,0,0], [-1,0,0], [1,0,0],  [1,0,0],  [1,0,0], [1,0,0], [0,0,1], [0,0,1], [0,0,1],   [0,0,1],
+                                          [0,0,-1], [0,0,-1], [0,0,-1], [0,0,-1] ) );
+
+                 // Those two lists, positions and normals, fully describe the "vertices".  What's the "i"th vertex?  Simply the combined
+                 // data you get if you look up index "i" of both lists above -- a position and a normal vector, together.  Now let's
+                 // tell it how to connect vertex entries into triangles.  Every three indices in this list makes one triangle:
+        this.indices.push( 0, 1, 2, 1, 3, 2, 4, 5, 6, 5, 7, 6, 8, 9, 10, 9, 11, 10, 12, 13,
+                          14, 13, 15, 14, 16, 17, 18, 17, 19, 18, 20, 21, 22, 21, 23, 22 );
+        // It stinks to manage arrays this big.  Later we'll show code that generates these same cube vertices more automatically.
+      }
+  }
+
+window.Cube_Outline = window.classes.Cube_Outline =
+class Cube_Outline extends Shape
+  { constructor()
+      { super( "positions", "colors" ); // Name the values we'll define per each vertex.
+
+        //  DONE (Requirement 5).
+                                // When a set of lines is used in graphics, you should think of the list entries as
+                                // broken down into pairs; each pair of vertices will be drawn as a line segment.
+
+        this.positions.push(...Vec.cast([-1,-1,-1], [-1,-1,1], [-1,-1,-1], [-1,1,-1], [-1,-1,-1], [1,-1,-1],
+                                        [1,1,-1],   [1,1,1],   [1,1,-1],   [1,-1,-1], [1,1,-1],   [-1,1,-1],
+                                        [-1,1,1],   [-1,1,-1], [-1,1,1],   [-1,-1,1], [-1,1,1],   [1,1,1],
+                                        [1,-1,1],   [1,-1,-1], [1,-1,1],   [1,1,1],   [1,-1,1],   [-1,-1,1]));
+        for(let i = 0; i<24; i++){this.colors.push(Color.of(1,1,1,1))};
+        this.indexed = false;       // Do this so we won't need to define "this.indices".
+      }
+  }
+
+window.Cube_Single_Strip = window.classes.Cube_Single_Strip =
+class Cube_Single_Strip extends Shape
+  { constructor()
+      { super( "positions", "normals" );
+
+        // TODO (Extra credit part I)
+        this.positions.push(...Vec.cast([-1,-1,-1], [1,-1,-1], [-1,-1,1], [1,-1,1], [1,1,-1],  [-1,1,-1],  [1,1,1],  [-1,1,1],
+                                          [-1,-1,-1], [-1,-1,1], [-1,1,-1], [-1,1,1], [1,-1,1],  [1,-1,-1],  [1,1,1],  [1,1,-1],
+                                          [-1,-1,1],  [1,-1,1],  [-1,1,1],  [1,1,1], [1,-1,-1], [-1,-1,-1], [1,1,-1], [-1,1,-1]));
+        this.normals.push(...Vec.cast( [0,-1,0], [0,-1,0], [0,-1,0], [0,-1,0], [0,1,0], [0,1,0], [0,1,0], [0,1,0], [-1,0,0], [-1,0,0],
+                                          [-1,0,0], [-1,0,0], [1,0,0],  [1,0,0],  [1,0,0], [1,0,0], [0,0,1], [0,0,1], [0,0,1],   [0,0,1],
+                                          [0,0,-1], [0,0,-1], [0,0,-1], [0,0,-1]));
+        this.indices.push();
+      }
+  }
+
+window.Vending_Machine = window.classes.Vending_Machine =
+class Vending_Machine extends Scene_Component
+  { constructor( context, control_box )     // The scene begins by requesting the camera, shapes, and materials it will need.
+      { super(   context, control_box );    // First, include a secondary Scene that provides movement controls:
+        if( !context.globals.has_controls   )
+          context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) );
+
+        const r = context.width/context.height;
+        context.globals.graphics_state.    camera_transform = Mat4.translation([ 5,-3,-30 ]);  // Locate the camera here (inverted matrix).
+        context.globals.graphics_state.projection_transform = Mat4.perspective( Math.PI/4, r, .1, 1000 );
+
+        const shapes = { 'box': new Cube(),               // At the beginning of our program, load one of each of these shape
+                       'strip': new Cube_Single_Strip(),  // definitions onto the GPU.  NOTE:  Only do this ONCE per shape
+                     'outline': new Cube_Outline() }      // design.  Once you've told the GPU what the design of a cube is,
+        this.submit_shapes( context, shapes );            // it would be redundant to tell it again.  You should just re-use
+                                                          // the one called "box" more than once in display() to draw
+                                                          // multiple cubes.  Don't define more than one blueprint for the
+                                                          // same thing here.
+        this.materials = {
+          clay: context.get_instance( Phong_Shader ).material( Color.of( 1,1,1, 1 ), { ambient: .6, diffusivity: .4 } ),
+          white: context.get_instance( Basic_Shader ).material()
+        }
+        this.lights = [ new Light( Vec.of(0,10,10,1), Color.of( 1, 1, 1, 1 ), 100000 ) ];
+      }
+    make_control_panel(){}
+    display( graphics_state ){
+      graphics_state.lights = this.lights;
+      let model_transform = Mat4.identity();
+      this.shapes.box.draw(graphics_state, model_transform.times(Mat4.scale(Vec.of(3.9, 7.2, 3.2))), this.materials.clay);
+    }
+  }
